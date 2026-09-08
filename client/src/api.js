773e10427f9314ae70b4d01e2c3f7901ab149ba5
Yripjar <rpriyanshu22@gmail.com>
2026-09-08 12:44:43 +0530

    Fix production CORS

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const baseURL = process.env.VITE_API_URL || 'http://localhost:5000/api';
+const baseURL =
+  import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
-  baseURL: baseURL,
+  baseURL,
 });
 
 export default api;
